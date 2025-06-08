689c5213c1dd842d52dd84015c64958ba4482ea3
Luis <usr3981@salesianos-lacuesta.net>
2025-06-08 11:19:00 +0100

    feat(Savefiles.java): Opcion para escribir en el fichero, creada correctamente

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import net.salesianos.file_options.save_files.SaveFiles;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        SaveFiles.createFile("fichero_1.txt", "hola");
+        SaveFiles.createFile();
 
     }
 }
--- src/net/salesianos/file_options/save_files/SaveFiles.java
+++ src/net/salesianos/file_options/save_files/SaveFiles.java
@@ -1,11 +1,15 @@
 package net.salesianos.file_options.save_files;
 
+import java.io.FileWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class SaveFiles {
 
-    public static void createFile(String nombreFichero, String textoFichero) {
+    public static void createFile() {
 
+        String textoFichero = "";
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Ingresa un texto (minimo 30 caracteres): ");
@@ -28,6 +32,19 @@ public class SaveFiles {
         // (A-Z,espacio-_), <- no se si es asi pero se ve chido :)
 
         textoFichero = textoFichero.toUpperCase().replace(" ", "_");
+
+        // Accedo al fochero, con FileWriter para poder escribir en el
+
+        try {
+
+            FileWriter writer = new FileWriter("src/net/salesianos/files/archivo.txt", StandardCharsets.UTF_8);
+
+            writer.write(textoFichero);
+
+            writer.close();
+        } catch (Exception e) {
+            System.out.println("Error porque no se encontro el archivo, FileWriter");
+        }
 
         scanner.close();
     }
