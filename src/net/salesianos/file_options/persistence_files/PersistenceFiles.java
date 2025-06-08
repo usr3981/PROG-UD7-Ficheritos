cfcde47e99d653ea86a4b8721d5a38896e51a9d3
Luis <usr3981@salesianos-lacuesta.net>
2025-06-08 19:03:11 +0100

    feat(PersistenceFiles.java): opcion para vizualizar todas las mesas agregada correctamente

--- src/net/salesianos/file_options/persistence_files/PersistenceFiles.java
+++ src/net/salesianos/file_options/persistence_files/PersistenceFiles.java
@@ -1,17 +1,16 @@
 package net.salesianos.file_options.persistence_files;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 
 import net.salesianos.tables.Table;
 
 public class PersistenceFiles {
 
     final static String PATH_FILE = "src/net/salesianos/files/archivo_persistencia.txt";
-
-    ArrayList<Table> tableList = new ArrayList<>();
 
     public static void createFile() {
 
@@ -45,7 +44,38 @@ public class PersistenceFiles {
         }
     }
 
-    public static void getTable() {
+    public static void getTables() {
+
+        String text = "";
+        String auxText = "";
+
+        try {
+
+            BufferedReader reader = new BufferedReader(
+                    new FileReader("src/net/salesianos/files/archivo_persistencia.txt", StandardCharsets.UTF_8));
+
+            // Valido que el contenido no sea null, y le asigno a un auxiliar el contenido
+            // en caso de no ser null para no sobreescribirlo
+
+            while (true) {
+
+                text = reader.readLine();
+
+                if (text != null) {
+                    auxText += text + "\n";
+                } else {
+                    break;
+                }
+            }
+
+            System.out.println(".: Listado de mesas :.\n");
+            System.out.println(auxText);
+
+            reader.close();
+
+        } catch (IOException e) {
+            System.out.println("Error no se encontro el archivo");
+        }
 
     }
 }
