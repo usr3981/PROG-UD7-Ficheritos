04cc978a56b46dba17ce063b4eca9aa5a6e57eb6
Luis <usr3981@salesianos-lacuesta.net>
2025-06-08 18:29:46 +0100

    feat(PersistenceFiles.java): metodo para crear el fichero creada correctamente

---
+++ src/net/salesianos/file_options/persistence_files/PersistenceFiles.java
@@ -0,0 +1,30 @@
+package net.salesianos.file_options.persistence_files;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+import net.salesianos.tables.Table;
+
+public class PersistenceFiles {
+
+    ArrayList<Table> tableList = new ArrayList<>();
+
+    public static void createFile() {
+
+        try {
+            FileWriter writer = new FileWriter("src/net/salesianos/files/archivo_persistencia.txt",
+                    StandardCharsets.UTF_8);
+
+            System.out.println("Archivo creado");
+
+            writer.close();
+
+        } catch (IOException e) {
+            System.out.println("Error al encontrar el archivo, File Writer");
+        }
+
+    }
+
+}
